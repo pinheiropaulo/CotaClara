a95a17c8c38a22710eec74d3971ad2b27ffab5dc
Paulo Pinheiro <pauloprap57@gmail.com>
2026-09-20 22:16:06 -0300

    feat(app): integra navegação entre login, início e cotas

---
+++ lib/app/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../features/auth/login_screen.dart';
+import '../features/home/home_screen.dart';
+import '../features/quotas/quotas_screen.dart';
+import 'routes/app_routes.dart';
+import 'theme/app_theme.dart';
+
+class CotaClaraApp extends StatelessWidget {
+  const CotaClaraApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'CotaClara',
+    debugShowCheckedModeBanner: false,
+    theme: AppTheme.dark,
+    initialRoute: AppRoutes.login,
+    routes: {
+      AppRoutes.login: (_) => const LoginScreen(),
+      AppRoutes.home: (_) => const HomeScreen(),
+      AppRoutes.quotas: (_) => const QuotasScreen(),
+    },
+  );
+}
